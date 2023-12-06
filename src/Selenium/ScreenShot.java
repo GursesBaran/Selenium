6e0873e6acb9f12b5d9863e71f4fc367a4154e7f
Baran Gurses <barangurses.bg@gmail.com>
2023-12-06 14:39:33 -0500

    Change

--- src/Selenium/ScreenShot.java
+++ src/Selenium/ScreenShot.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
